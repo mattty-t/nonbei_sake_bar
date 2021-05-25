2b64ccddf1b211a20bd5d4c6574c89ac2f70b761
mattty-t <mattty.tran@gmail.com>
2021-05-26 01:18:39 +1000

    deleted unwated code

--- drinks.rb
+++ drinks.rb
@@ -3,9 +3,9 @@
 class Sake
     attr_accessor :type, :size, :price, :description
     
-    def initialize(type, quantity, price, description)
+    def initialize(type, size, price, description)
         @type = type                #type of sake 
-        @size = size        #displays the sizes of available : 150ml, 250ml, btl 
+        @size = size        #displays the size of sake  
         @price = price              #price of each size 
         @description = description  #description of the sake 
     end 
@@ -31,11 +31,7 @@ class Sake
      # displays the price 
      def display_price 
         puts 
-        puts "Sizes Available: #{@price}"
+        puts "#{@price} AUD"
     end 
 
 end 
-
-dewazakura_sparkling = Sake.new("Dewazakura Sparkling Sake", "125ml", "50", "This sparkling sake is clear, light bodied but with more sake character than the ‘fruitier’ styles available. A hint of herb on nose, elegant on palate with delicate dry finish.")
-amanoto_ginjo = Sake.new("Amanoto Junmai Ginjo", "150ml", "75", "This Junmai Ginjo has beautiful purity with subtle yeast complexity on the nose. Elegant and fruity but powerful on the palate with a crisp clean finish with zesty citrus.")
-houraisen_bi = Sake.new("Houraisen bi", "150ml", "100", "Bi is one of Houraisen’s flagship sake’s. It is a very elegant style of Junmai Daiginjo. It is tank matured for 10 months at 10 degrees in order to have a rounder and more mellow character. It has a nose that indicates sweet fruits. The palate shows a good balance between the gentle acidity and the sweet fruit flavours.")
